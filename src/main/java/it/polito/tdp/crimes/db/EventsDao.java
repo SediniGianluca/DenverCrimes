af097531fe5426cc05ed5cfa42c3722968797eb8
Gianluca Sedini <s248740@studenti.polito.it>
2021-06-05 12:24:16 +0200

    risoluzione

--- src/main/java/it/polito/tdp/crimes/db/EventsDao.java
+++ src/main/java/it/polito/tdp/crimes/db/EventsDao.java
@@ -5,12 +5,56 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
+
+import org.jgrapht.Graph;
+import org.jgrapht.graph.DefaultWeightedEdge;
+
+import it.polito.tdp.crimes.model.Arco;
 import it.polito.tdp.crimes.model.Event;
 
 
 public class EventsDao {
-	
+
+	public List<Integer> getMesi(){
+		String sql = "SELECT DISTINCT Month(reported_date) as mese FROM events";
+		List<Integer> mesi = new LinkedList<>();
+		try {
+			Connection conn = DBConnect.getConnection() ;
+			PreparedStatement st = conn.prepareStatement(sql) ;
+			ResultSet res = st.executeQuery() ;
+			while (res.next()) {
+				mesi.add(res.getInt("mese"));
+			}
+			conn.close();
+			Collections.sort(mesi);
+			return mesi;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null ;
+		}
+	}
+	public List<String> getCategorie(){
+		String sql = "SELECT DISTINCT offense_category_id as categoria FROM events";
+		List<String> categorie = new LinkedList<>();
+		try {
+			Connection conn = DBConnect.getConnection() ;
+			PreparedStatement st = conn.prepareStatement(sql) ;
+			ResultSet res = st.executeQuery() ;
+			while (res.next()) {
+				categorie.add(res.getString("categoria"));
+			}
+			conn.close();
+			return categorie;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null ;
+		}
+	}
 	public List<Event> listAllEvents(){
 		String sql = "SELECT * FROM events" ;
 		try {
@@ -53,5 +97,67 @@ public class EventsDao {
 			return null ;
 		}
 	}
-
+	
+	public List<String> getVertici(String categoria, int mese){
+		String sql = "SELECT DISTINCT offense_type_id " 
+				+ "FROM events "
+				+"WHERE offense_category_id = ? AND Month(reported_date)=? ";
+		List<String> result = new LinkedList<String>();
+		try {
+			Connection conn = DBConnect.getConnection() ;
+			PreparedStatement st = conn.prepareStatement(sql) ;
+			st.setString(1, categoria);
+			st.setInt(2, mese);
+			
+			ResultSet res = st.executeQuery() ;
+			while(res.next()) {
+				result.add(res.getString("offense_type_id"));
+			}
+			st.close();
+			res.close();
+			conn.close();
+			return result;
+			
+	} catch (SQLException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+		return null ;
+		}
+	}
+	
+	public List<Arco> getArchi(String categoria,int mese){
+		String sql = "SELECT e1.offense_type_id as v1, e2.offense_type_id as v2 , COUNT(DISTINCT(e1.neighborhood_id)) as peso " 
+				+ "FROM events e1, events e2 "
+				+"WHERE e1.offense_category_id = ? AND e1.offense_category_id =e2.offense_category_id "
+				+ "AND Month(e1.reported_date)=? AND Month(e1.reported_date)=Month(e2.reported_date) "
+				+ "AND e1.offense_type_id > e2.offense_type_id "
+				+ "AND e1.neighborhood_id = e2.neighborhood_id "
+				+ "GROUP BY e1.offense_type_id , e2.offense_type_id ";
+		
+		List<Arco> result = new LinkedList<Arco>();
+		
+		try {
+			Connection conn = DBConnect.getConnection() ;
+			PreparedStatement st = conn.prepareStatement(sql) ;
+			st.setString(1, categoria);
+			st.setInt(2, mese);
+			
+			ResultSet res = st.executeQuery() ;
+			
+			while(res.next()) {
+				result.add(new Arco(res.getString("v1"),res.getString("v2"),res.getInt("peso")));
+			}
+			st.close();
+			res.close();
+			conn.close();
+			return result;
+			
+	} catch (SQLException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+		return null ;
+		}
+	}
 }
+
+
--- src/main/java/it/polito/tdp/crimes/model/Model.java
+++ src/main/java/it/polito/tdp/crimes/model/Model.java
@@ -1,6 +1,88 @@
 package it.polito.tdp.crimes.model;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.jgrapht.Graphs;
+import org.jgrapht.graph.DefaultWeightedEdge;
+import org.jgrapht.graph.SimpleWeightedGraph;
+
+import it.polito.tdp.crimes.db.EventsDao;
 
 public class Model {
 	
+	private SimpleWeightedGraph<String,DefaultWeightedEdge> grafo;
+	private EventsDao dao;
+	private List<String> best;
+	public Model() {
+		dao=new EventsDao();
+	}
+	
+	public List<Integer> getMesi(){
+		return dao.getMesi();
+	}
+
+	public List<String> getCategorie(){
+		return dao.getCategorie();
+	}
+	
+	public void creaGrafo(String categoria , int mese) {
+		this.grafo= new SimpleWeightedGraph<>(DefaultWeightedEdge.class);
+		
+		Graphs.addAllVertices(grafo, dao.getVertici(categoria,mese));
+		
+		for(Arco a: dao.getArchi(categoria, mese)) {
+			if(this.grafo.getEdge(a.getV1(), a.getV2())==null) {
+				Graphs.addEdgeWithVertices(grafo,a.getV1(), a.getV2(), a.getPeso());
+			}
+		}
+		System.out.println("# Vertici :"+this.grafo.vertexSet().size());
+		System.out.println("# aArchi :"+this.grafo.edgeSet().size());
+	}
+	public List<Arco> getArchiMaggioriMedia(){
+		double pesoMedio =0.0;	
+		for(DefaultWeightedEdge e : this.grafo.edgeSet()) {
+			pesoMedio+= this.grafo.getEdgeWeight(e);
+		}
+		pesoMedio=pesoMedio/this.grafo.edgeSet().size();
+		
+		List<Arco> result = new LinkedList<>();
+		for(DefaultWeightedEdge e : this.grafo.edgeSet()) {
+			if(this.grafo.getEdgeWeight(e)>pesoMedio)
+				result.add(new Arco(this.grafo.getEdgeSource(e),this.grafo.getEdgeTarget(e),this.grafo.getEdgeWeight(e)));
+			}
+		return result;
+		}
+	public List<String> trovaPercorso(String sorgente, String destinazione) {
+		List<String> parziale = new ArrayList<>();
+		this.best = new ArrayList<>();
+		parziale.add(sorgente);
+		trovaRiscorsivo(destinazione,parziale, 0);
+		return this.best;
+	}
+
+	private void trovaRiscorsivo(String destinazione, List<String> parziale, int L) {
+
+		//CASO TERMINALE? -> quando l'ultimo vertice inserito in parziale è uguale alla destinazione
+		if(parziale.get(parziale.size() - 1).equals(destinazione)) {
+			if(parziale.size() > this.best.size()) {
+				this.best = new ArrayList<>(parziale);
+			}
+			return;
+		}
+
+		//scorro i vicini dell'ultimo vertice inserito in parziale
+		for(String vicino : Graphs.neighborListOf(this.grafo, parziale.get(parziale.size() -1 ))) {
+			//cammino aciclico -> controllo che il vertice non sia già in parziale
+			if(!parziale.contains(vicino)) {
+				//provo ad aggiungere
+				parziale.add(vicino);
+				//continuo la ricorsione
+				this.trovaRiscorsivo(destinazione, parziale, L+1);
+				//faccio backtracking
+				parziale.remove(parziale.size() -1);
+			}
+		}
+	}
 }
